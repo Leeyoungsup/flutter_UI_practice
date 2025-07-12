68174e1f97864820985dda6e174b4ae8b215a664
Leeyoungsup <liive29252@gmail.com>
2025-07-12 18:48:44 +0900

    Dynamic Type

--- Dart_practice.dart
+++ Dart_practice.dart
@@ -1,7 +1,16 @@
 // ignore: file_names
 void main(){
-  var name='하리보';
+  var name;
   name='Haribo';
-  print("Hello, $name!");
-
+  name=12;
+  name=true;
+  if(name is String){
+    print("String: $name");
+  } else if(name is int){
+    print("int: $name");    
+  } else if(name is bool){
+    print("bool: $name");
+  } else {
+    print("Unknown type");
+  }
 }
