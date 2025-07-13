885da995255275a206b4ae38044258c774a7d9cc
Leeyoungsup <liive29252@gmail.com>
2025-07-13 15:02:23 +0900

    Lists

--- Dart_practice.dart
+++ Dart_practice.dart
@@ -1,19 +1,8 @@
 // ignore: file_names
-import 'dart:ffi';
+
 
 void main(){
-  String name = 'Dart';
-  int age = 10;
-  double height = 1.75;
-  bool isStudent = true;
-  var city = 'Seoul';
-  const pi = 3.14;
-  final currentTime = DateTime.now();
-  print('Hello, $name!');
-  print('Age: $age');
-  print('Height: $height m');
-  print('Is Student: $isStudent');
-  print('City: $city');
-  print('Pi: $pi');
-  print('Current Time: $currentTime');
+  bool giveMeFive= true;
+  List<int> numbers= [1, 2, 3, 4, if(giveMeFive) 5,];
+  print(numbers);
 }
