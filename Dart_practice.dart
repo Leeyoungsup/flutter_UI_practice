b1a0b14dd3e92bdc0361c370ca8392318504db14
Leeyoungsup <liive29252@gmail.com>
2025-07-13 14:37:23 +0900

    Late Variables

--- Dart_practice.dart
+++ Dart_practice.dart
@@ -1,11 +1,8 @@
+// ignore: file_names
 void main(){
-  String? name = 'Flutter';
-  name=null;
-  if (name != null) {
-    print('Hello, $name!');
-  } else {
-    print('Name is null');
-  }
-  name?.isEmpty;
-
+  late final String? name;
+  // print(name); // 선언 전에 접근하면 오류 발생
+  name = null;
+  // name='Dart'; //단 한번만 초기화 가능
+  print("Hello, $name!");
 }
