2bc52856e637aa08f92557193dc5380360a846ea
Leeyoungsup <liive29252@gmail.com>
2025-07-17 15:57:51 +0900

    위치 서비스 및 Google Maps 관련 코드 제거: AndroidManifest.xml, AppDelegate.swift, Info.plist, main.dart, pubspec.yaml 수정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Google Maps API 키 설정 (실제 API 키로 교체 필요)
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
